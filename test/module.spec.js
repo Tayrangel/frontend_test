4f47b7a602c0f21c9a1b568e030acb19e9881a13
Tayhana Marçal Rangel da Fonseca <tayhanarangel@gmail.com>
2021-11-08 13:07:28 -0300

    Update
    Update

--- test/module.spec.js
+++
@@ -1,8 +0,0 @@
-/*import { toContain } from '@nuxt/test-utils'
-import faker from 'faker';
-
-const tweet = require('../plugins/mirage');
-
-test('to contain name in tweet', () => {
-  expect(tweet).toContain('{name}');
-});*/
--- test/url.spec.js
+++
@@ -1,9 +0,0 @@
-import { url, setupTest } from '@nuxt/test-utils'
-
-describe('ssr', () => {
-  setupTest({ server: true })
-
-  it('renders the index page', async () => {
-    const thePage = url('/page')
-  })
-})
